b2af64c812b09cb6d3648f68316e54f55988eb00
Muhammmadhashim-rgb <muhammadhashim4412@gmail.com>
2025-10-08 23:03:11 +0500

    Home page complete

--- src/App.js
+++ src/App.js
@@ -4,35 +4,27 @@ import HeroSection from './non-repetitiveComponents/HeroSection';
 import NEWdropsHeading from './non-repetitiveComponents/NEWdropsHeading';
 import Categories from './non-repetitiveComponents/Categories'
 import Reviews from './non-repetitiveComponents/Reviews';
+import Footer from './repetitiveComponents/Footer';
 
 
 function App() {
   return (
     <div className='Home_page'>
       <div className=" container mx-auto">
-
         <Navbar />
-
-
         <HeroSection />
-
-
         <NEWdropsHeading />
-
-
-
       </div>
-
       <div>
-
         <Categories />
-
       </div>
-
       <div className="container mx-auto">
         <Reviews />
       </div>
-
+      <div className="container  mx-auto mt-[12%]">
+        <Footer/> 
+      </div>
+     
     </div>
 
 
